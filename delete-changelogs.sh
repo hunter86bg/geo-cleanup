8c5e7d4066f71540ed1d0f01595ff4f6fb5b0ec3
hunter86bg <hunter86_bg@yahoo.com>
2021-09-20 23:17:55 +0300

    Use /usr/bin/env in order to not specify the full path of the binaries

--- delete-changelogs.sh
+++ delete-changelogs.sh
@@ -4,7 +4,7 @@ source_vol="primevol"
 dest_vol="slavevol"
 dest_node="slave1"
 brick_path="/gluster/brick1/brick1"
-brick_path_dashes=$(echo $brick_path | sed 's#/##' | sed 's#/#-#g')
+brick_path_dashes=$(/usr/bin/env echo $brick_path | /usr/bin/env sed 's#/##' | /usr/bin/env sed 's#/#-#g')
 
 
 while [[ $# -gt 0 ]]; do
@@ -23,9 +23,9 @@ done
 #/var/lib/misc/gluster/gsyncd/${source_vol}_${dest_node}_${dest_vol}/${brick_path_dashes}/.processed
 
 # We are using tmpfs as processing is faster
-mkdir /var/tmp/gluster_cleanup
+/usr/bin/env mkdir /var/tmp/gluster_cleanup
 #TMPFS is hardcoded to 100M as we don't need much space
-mount -t tmpfs -o size=100M - /var/tmp/gluster_cleanup
+/usr/bin/env mount -t tmpfs -o size=100M - /var/tmp/gluster_cleanup
 
 # Temp files which will be cleaned up on exit, ctrl+C ,etc
 changelog_file=$(mktemp /var/tmp/gluster_cleanup/changeloglist.XXXXXX)
@@ -33,9 +33,9 @@ delete_list=$(mktemp /var/tmp/gluster_cleanup/todelete.XXXXXX)
 processed_changelogs=$(mktemp /var/tmp/gluster_cleanup/processed.XXXXXX)
 
 function cleanup {
-	rm -f $changelog_file $delete_list $processed_changelogs
-	umount /var/tmp/gluster_cleanup
-	rmdir /var/tmp/gluster_cleanup
+	/usr/bin/env rm -f $changelog_file $delete_list $processed_changelogs
+	/usr/bin/env umount /var/tmp/gluster_cleanup
+	/usr/bin/env rmdir /var/tmp/gluster_cleanup
 }
 
 # Actual code that captures exit/Ctrl+C,etc
@@ -43,17 +43,17 @@ trap cleanup EXIT SIGINT SIGKILL SIGQUIT SIGTERM
 
 # Get all changelogs locally and store them into $changelog_file
 # Changelogs are different per host
-ionice -c 2 -n 7 find ${brick_path}/.glusterfs/changelogs/$(date '+%Y')/  -type f -name "CHANGELOG.*" -print > ${changelog_file}
+/usr/bin/env ionice -c 2 -n 7 /usr/bin/env find ${brick_path}/.glusterfs/changelogs/$(date '+%Y')/  -type f -name "CHANGELOG.*" -print > ${changelog_file}
 
 #Obtain all processed changelogs
-PROCESSED_LOCALLY=$(ionice -c 2 -n 7 find /var/lib/misc/gluster/gsyncd/${source_vol}_${dest_node}_${dest_vol}/${brick_path_dashes}/.processed/ -type f -name "archive*.tar" |  wc -l)
+PROCESSED_LOCALLY=$(/usr/bin/env ionice -c 2 -n 7 /usr/bin/env find /var/lib/misc/gluster/gsyncd/${source_vol}_${dest_node}_${dest_vol}/${brick_path_dashes}/.processed/ -type f -name "archive*.tar" |  wc -l)
 
 if [ "$PROCESSED_LOCALLY" -gt 0 ]; then
 
-	tar -tvf /var/lib/misc/gluster/gsyncd/${source_vol}_${dest_node}_${dest_vol}/${brick_path_dashes}/.processed/archive_*.tar | awk '{print $6}' | head -n -5 >> ${processed_changelogs}
+	/usr/bin/env tar -tvf /var/lib/misc/gluster/gsyncd/${source_vol}_${dest_node}_${dest_vol}/${brick_path_dashes}/.processed/archive_*.tar | /usr/bin/env awk '{print $6}' | /usr/bin/env head -n -5 >> ${processed_changelogs}
 
 else
-	echo "No changelogs were processed locally!" >&2
+	/usr/bin/env echo "No changelogs were processed locally!" >&2
 	exit 0
 fi
 
@@ -61,19 +61,19 @@ fi
 # In the end if a changelog that was in the .proccessed/archive_<year><month>.tar, we can safely delete it
 # So we add it to a list of files that can be deleted
 while read changelog; do 
-	grep $changelog ${changelog_file}
+	/usr/bin/env grep $changelog ${changelog_file}
 done < ${processed_changelogs}  >> ${delete_list}
 
 # This is just for info in case you are interested how many changelogs we got and how many were processed
 CHANGELOG_FILE_COUNT=$(wc -l $changelog_file | awk '{print $1}')
 ENTRIES_TO_DELETE=$(wc -l $delete_list | awk '{print $1}')
 
-echo "CHANGELOG FILE COUNT: $CHANGELOG_FILE_COUNT"
-echo "ENTRIES TO DELETE: $ENTRIES_TO_DELETE"
+/usr/bin/env echo "CHANGELOG FILE COUNT: $CHANGELOG_FILE_COUNT"
+/usr/bin/env echo "ENTRIES THAT ARE NOW DELETED: $ENTRIES_TO_DELETE"
 
 # If no logs have to be deleted , we skip
 if [ "$ENTRIES_TO_DELETE" -gt 0 ]; then
-	$DRYRUN	xargs --arg-file="$delete_list" rm
+	$DRYRUN	/usr/bin/env xargs --arg-file="$delete_list" rm
 else
-	echo "Nothing to do!"
+	/usr/bin/env echo "Nothing to do!"
 fi
